f59a2e06f41e8e644264a17c9279912d773d2398
Sheldon Hearn <sheldonh@starjuice.net>
2014-08-25 11:13:25 +0200

    Simplify

--- spec/service_registry_spec.rb
+++ spec/service_registry_spec.rb
@@ -1,101 +1,124 @@
 require 'spec_helper'
 
-$VERBOSE = 0
+$VERBOSE = nil
 require 'zk'
 require 'json'
+
+module KeywordSupport
+  def self.require!(scope, *keywords)
+    (keywords.empty? ? enumerate(scope) : keywords).each do |keyword|
+      eval(%Q{ #{keyword} or raise ArgumentError.new("missing #{keyword} keyword") }, scope)
+    end
+  end
+
+  def self.import!(scope, *keywords)
+    (keywords.empty? ? enumerate(scope) : keywords).each do |keyword|
+      eval(%Q{ @#{keyword} = #{keyword} or raise ArgumentError.new("missing #{keyword} keyword") }, scope)
+    end
+  end
+
+  def self.enumerate(scope)
+    eval(%q{ method(__method__).parameters.select { |type, name| type == :key }.map { |type, name| name } }, scope)
+  end
+end
 
 module ServiceDiscovery
 
   # API
   class ServiceRegistry
+
     def initialize(environment: nil, provider: provider)
-      @environment = environment or raise ArgumentError.new("missing environment keyword")
-      @provider = provider
+      KeywordSupport.import! binding
     end
 
     def register_service(service_context: nil, instance: nil, uri: nil)
-      service_context or raise ArgumentError.new("missing service_context keyword")
-      instance or raise ArgumentError.new("missing uri keyword")
-      uri or raise ArgumentError.new("missing uri keyword")
+      KeywordSupport.require! binding
 
       @provider.register(environment: @environment, service_context: service_context, instance: instance, uri: uri)
     end
 
     def lookup_service_uri(service_context: nil)
-      service_context or raise ArgumentError.new("missing service_context keyword")
+      KeywordSupport.require! binding
 
-      uri = @provider.lookup(environment: @environment, service_context: service_context)
+      @provider.lookup(environment: @environment, service_context: service_context).sample[:uri]
     end
 
-    def connect_service_provider(provider_class: nil)
-      provider_class or raise ArgumentError.new("missing provider_class keyword")
+    def deregister_service(service_context: nil, instance: nil)
+      KeywordSupport.require! binding
 
-      service_context = provider_class.service_context
-      uri = @provider.lookup(environment: @environment, service_context: service_context)
-      service_registration = OldServiceRegistration.new(uri: uri, registry: self)
-      provider_class.new(service_registration: service_registration)
+      @provider.deregister(environment: @environment, service_context: service_context, instance: instance)
+    end
+
+  end
+
+  class ServiceRegistration
+    attr_reader :uri
+
+    def initialize(uri: nil, provider: nil)
+      KeywordSupport.import! binding
+    end
+
+    def deregister
+      @provider.deregister
     end
   end
 
   class ServiceContext
-
     attr_reader :domain_perspective, :service, :protocol
 
     def initialize(domain_perspective: nil, service: nil, protocol: nil)
-      @domain_perspective = domain_perspective or raise ArgumentError.new("missing domain_perspective keyword")
-      @service = service or raise ArgumentError.new("missing service keyword")
-      @protocol = protocol or raise ArgumentError.new("missing protocol keyword")
+      KeywordSupport.import! binding
     end
-
-  end
-
-  class OldServiceRegistration
-
-    attr_reader :uri, :registry
-
-    def initialize(uri: nil, registry: nil)
-      @uri = uri or raise ArgumentError.new("missing uri keyword")
-      @registry = registry or raise ArgumentError.new("missing registry keyword")
-    end
-
   end
 
   module Provider
 
     class ZooKeeperServiceRegistry
 
+      class ZooKeeperRegistration
+        def initialize(zoo_keeper, znode)
+          @znode = znode
+          @zoo_keeper = zoo_keeper
+        end
+
+        def deregister
+          @zoo_keeper.delete(@znode, :ignore => :no_node)
+        end
+      end
+
       def initialize(hosts: hosts)
-        @hosts = hosts or raise ArgumentError.new("missing hosts keyword")
+        KeywordSupport.import! binding
       end
 
       def register(environment: nil, service_context: nil, instance: nil, uri: nil)
-        environment or raise ArgumentError.new("missing environment keyword")
-        service_context or raise ArgumentError.new("missing service_context keyword")
-        instance or raise ArgumentError.new("missing instance keyword")
-        uri or raise ArgumentError.new("missing uri keyword")
+        KeywordSupport.require! binding
 
         zk = ZK.new(@hosts)
+
         path = instances_path(environment, service_context)
         path.split("/").inject do |current, child|
           (current + "/" + child).tap { |znode| zk.create(znode, :ignore => :node_exists) }
         end
+
         instance_path = path + "/" + instance
         zk.create(instance_path, {uri: uri}.to_json, mode: :ephemeral)
 
-        ZooKeeperServiceRegistration.new(znode: instance_path, zoo_keeper: zk)
-      end
-
-      def deregister
-        raise "not registered" unless registered?
-        @zk.delete("/service/#{@service}/#{@environment}/instance/#{@instance}")
-        @zk = nil
+        ServiceRegistration.new(uri: uri, provider: ZooKeeperRegistration.new(zk, instance_path))
       end
 
       def lookup(environment: nil, service_context: nil)
-        environment or raise ArgumentError.new("missing environment keyword")
-        service_context or raise ArgumentError.new("missing service_context keyword")
-        # do zookeeper stuff
-        'https://crm.starjuice.net/client'
+        KeywordSupport.require! binding
+
+        zk = ZK.new(@hosts)
+        path = instances_path(environment, service_context)
+        zk.children(path).map { |instance| JSON.parse(zk.get(path + "/" + instance)[0], symbolize_names: true) }
+      end
+
+      def deregister(environment: nil, service_context: nil, instance: nil)
+        KeywordSupport.require! binding
+
+        zk = ZK.new(@hosts)
+        zk.delete(instances_path(environment, service_context) + "/" + instance, :ignore => :no_node)
       end
 
       private
@@ -116,15 +139,15 @@ end
 class ContactServiceRestProvider
 
   def self.service_context
-    ServiceDiscovery::ServiceContext.new(domain_perspective: 'crm', service: 'contact_details', protocol: 'http')
+    ServiceDiscovery::ServiceContext.new(domain_perspective: 'crm', service: 'contacts', protocol: 'http')
   end
 
   def initialize(service_registration: nil)
-    @service_registration = service_registration or raise ArgumentError.new("missing service_registration keyword")
+    KeywordSupport.import! binding
   end
 
   def get_contacts(client_number: nil)
-    client_number or raise new ArgumentError.new("missing client_number keyword")
+    KeywordSupport.require! binding
     # rest = Rest.new(@service_location)
     # json = rest.get("/client/#{client_number}/contacts")
     # JSON.parse(json).each do { |properties| Contact.new(properties: properties) }
@@ -141,11 +164,11 @@ end
 class ContactService
 
   def initialize(provider: nil)
-    @provider = provider or raise ArgumentError.new("missing provider keyword")
+    KeywordSupport.import! binding
   end
 
   def get_invoice_recipients(client_number: nil)
-    client_number or raise ArgumentError.new("missing client_number keyword")
+    KeywordSupport.require! binding
     contacts = @provider.get_contacts(client_number: client_number)
     contacts.select { |c| c.role == 'billing' }
   end
@@ -157,7 +180,7 @@ class Contact
   attr_reader :email, :role
 
   def initialize(properties: nil)
-    properties or raise ArgumentError.new("missing properties keyword")
+    KeywordSupport.require! binding
     @email = properties[:email]
     @role = properties[:role]
   end
@@ -170,6 +193,10 @@ module ServiceDiscovery
 
     before(:each) do
       raise "ZK_HOST environment variable must point to test ZooKeeper instance:port" unless ENV['ZK_HOST']
+    end
+
+    after(:each) do
+      subject.deregister_service(service_context: service_context, instance: 'crm1')
     end
 
     let(:service_context) { ServiceDiscovery::ServiceContext.new(domain_perspective: 'crm', service: 'contact_details', protocol: 'http') }
@@ -185,15 +212,9 @@ module ServiceDiscovery
     end
 
     it "looks up a service uri" do
+      subject.register_service(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
       uri = subject.lookup_service_uri(service_context: service_context)
       expect(uri).to eql 'https://crm.starjuice.net/client'
-    end
-
-    it "creates a connected service provider" do
-      provider = subject.connect_service_provider(provider_class: ContactServiceRestProvider)
-      service = ContactService.new(provider: provider)
-      contacts = service.get_invoice_recipients(client_number: 'C1234576890')
-      expect(contacts.first.email).to eql 'sheldonh@starjuice.net'
     end
 
   end
