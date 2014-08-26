b0f0f7c783a392b46876bc51eb305dc9b6cb11cb
Sheldon Hearn <sheldonh@starjuice.net>
2014-08-26 11:10:23 +0200

    Clarify instance name

--- spec/service_registry_spec.rb
+++ spec/service_registry_spec.rb
@@ -16,7 +16,7 @@ module ServiceDiscovery
     end
 
     after(:each) do
-      subject.deregister_service(service_context: service_context, instance: 'crm1')
+      subject.deregister_service(service_context: service_context, instance: 'server1')
     end
 
     let(:service_context) { ServiceDiscovery::ServiceContext.new(domain_perspective: 'crm', service: 'contact_details', protocol: 'http') }
@@ -27,29 +27,29 @@ module ServiceDiscovery
     end
 
     it "registers a service uri" do
-      registration = subject.register_service_uri(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
+      registration = subject.register_service_uri(service_context: service_context, instance: 'server1', uri: 'https://crm.starjuice.net/client')
       expect(registration.uri).to eql 'https://crm.starjuice.net/client'
     end
 
     it "looks up a service uri" do
-      subject.register_service_uri(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
+      subject.register_service_uri(service_context: service_context, instance: 'server1', uri: 'https://crm.starjuice.net/client')
       uri = subject.lookup_service_uri(service_context: service_context)
       expect(uri).to eql 'https://crm.starjuice.net/client'
     end
 
     it "deregisters a service uri" do
-      subject.register_service_uri(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
-      subject.deregister_service(service_context: service_context, instance: 'crm1')
+      subject.register_service_uri(service_context: service_context, instance: 'server1', uri: 'https://crm.starjuice.net/client')
+      subject.deregister_service(service_context: service_context, instance: 'server1')
     end
 
     it "provides a service registration capable of deregistration" do
-      registration = subject.register_service_uri(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
+      registration = subject.register_service_uri(service_context: service_context, instance: 'server1', uri: 'https://crm.starjuice.net/client')
       registration.deregister
       expect(subject.lookup_service_uri(service_context: service_context)).to be_nil
     end
 
     it "automatically deregisters a crashed service" do
-      registration = subject.register_service_uri(service_context: service_context, instance: 'crm1', uri: 'https://crm.starjuice.net/client')
+      registration = subject.register_service_uri(service_context: service_context, instance: 'server1', uri: 'https://crm.starjuice.net/client')
       registration.send(:provider).send(:zoo_keeper).close! # simulate socket close
       expect(subject.lookup_service_uri(service_context: service_context)).to be_nil
     end
